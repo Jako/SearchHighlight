d85a66486917119f0b0752e26262445548879072
Thomas Jakobi <thomas.jakobi@partout.info>
2015-11-26 16:26:24 +0100

    Initial release for MODX Revolution

---
+++ core/components/searchhighlight/lexicon/en/default.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Default English Lexicon Entries for SearchHighlight
+ *
+ * @package searchhighlight
+ * @subpackage lexicon
+ */
+$_lang['searchhighlight'] = 'SearchHighlight';
+$_lang['searchhighlight.search_terms'] = 'Search Terms';
+$_lang['searchhighlight.remove_highlighting'] = 'Remove Highlighting';
+
+$_lang['setting_searchhighlight.highlightclasses'] = 'Search Highlight CSS Classnames';
+$_lang['setting_searchhighlight.highlightclasses_desc'] = 'CSS classnames that are assigned to the found text on the current page.';
